7fe693d6522cd54bcecf6ac6762eae2f35edc8c6
Pao1a <paolaagonzalezm@email.arizona.edu>
2021-07-29 14:58:51 -0700

    Setting up initial Apollo files

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,15 @@
+const { ModelName } = require("../models");
+
+const resolvers = {
+
+    Query: {
+
+    },
+
+    Mutation: {
+        
+    }
+
+}
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,12 @@
+const { gql } = require('apollo-server-express');
+
+const typeDerfs= gql`
+    type 
+
+
+
+`;
+
+
+
+module.exports = typeDefs;
