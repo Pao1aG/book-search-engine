cf8f38db7ea578e7b328485ec0ac417415d28160
Pao1a <paolaagonzalezm@email.arizona.edu>
2021-07-30 16:41:59 -0700

    modified SearchBooks.js with mutation

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -5,6 +5,11 @@ import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
+// Use the Apollo useMutation() Hook to execute the SAVE_BOOK mutation in the handleSaveBook() function 
+//instead of the saveBook() function imported from the API file.
+
+// Make sure you keep the logic for saving the book's ID to state in the try...catch block!
+
 const SearchBooks = () => {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
@@ -52,10 +57,16 @@ const SearchBooks = () => {
     }
   };
 
+  //TODO
+  /* Use the Apollo useMutation() Hook to execute the SAVE_BOOK mutation in the handleSaveBook() function 
+  instead of the saveBook() function imported from the API file. */
+  
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    const [saveBook, { error }] = useMutation(SAVE_BOOK);
+    // save book through mutation
 
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -65,11 +76,16 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      const { data } = await saveBook({
+        variables: {
+          authors,
+          description,
+          bookId,
+          image,
+          link,
+          title
+        }
+      })
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
